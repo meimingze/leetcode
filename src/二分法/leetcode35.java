34ad631b25fa074b0061683c9bc292562152ddb8
2489433378@qq.com <2489433378@qq.com>
2023-02-28 18:43:40 +0800

    Initial commit

---
+++ src/二分法/leetcode35.java
@@ -0,0 +1,30 @@
+package 二分法;//35. 搜索插入位置
+//给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+//请必须使用时间复杂度为 O(log n) 的算法。
+//输入: nums = [1,3,5,6], target = 5
+  //      输出: 2
+/**
+ *二分查找的一个小变化，没有难度
+ */
+
+public class leetcode35 {
+}
+class Solution35 {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length -1 ;
+        while(left <= right){
+            int mid = (right - left) / 2 + left;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if (nums[mid] < target){
+                left = mid + 1;
+            }
+            if(nums[mid] > target){
+                right = right -1;
+            }
+        }
+        return left;
+    }
+}
