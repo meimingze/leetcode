31bc0dd0654e8df03a4bd9a8581cbc0ab1773461
2489433378@qq.com <2489433378@qq.com>
2023-05-31 10:47:27 +0800

    Initial commit

--- src/哈希表/leetcode15.java
+++ src/哈希表/leetcode15.java
@@ -1,8 +1,54 @@
 package 哈希表;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * @auther XMZ
  * @create 2023--30-下午4:18
  */
 public class leetcode15 {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > 0 ){
+                return res;
+            }
+            if(i > 0 && nums[i] == nums[i -1]){
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (right > left){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum > 0){
+                    right--;
+                }
+                if(sum < 0){
+                    left++;
+                }
+                if(sum == 0){
+                    res.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                }
+                //去重应该在找到一个三元组之后
+                while(right > left && nums[right] == nums[right - 1]){
+                    right--;
+                }
+                while(right > left && nums[left] == nums[left + 1] ){
+                    left++;
+                }
+                left ++;
+                right --;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+        System.out.println(threeSum(nums));
+    }
 }
---
+++ src/哈希表/leetcode18.java
@@ -0,0 +1,8 @@
+package 哈希表;
+
+/**
+ * @auther XMZ
+ * @create 2023--31-上午10:47
+ */
+public class leetcode18 {
+}
