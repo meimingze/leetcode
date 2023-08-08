27177af949768a9a9895978bc4788300b463575c
有人相爱，有人夜里开车看海，有人leetcode第一题都做不出来 <105974183+meimingze@users.noreply.github.com>
2023-08-08 20:23:16 +0800

    Add files via upload

---
+++ leetcode1749.java
@@ -0,0 +1,20 @@
+public class leetcode1749 {
+    public int maxSubArray(int nums[])
+    {
+        int length = nums.length;
+        int dp[] = new int[length];
+        dp[0] = nums[0];
+        int result = Integer.MIN_VALUE;
+        for(int i =1 ; i< nums.length; i++)
+        {
+            if(dp[i -1] >0)
+            {
+                dp[i] += dp[i-1] + nums[i];
+            }else{
+                dp[i] = nums[i];
+            }
+            result = Math.max(result,dp[i]);
+        }
+        return result;
+     }
+}
