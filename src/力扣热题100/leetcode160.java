e0d92b03e68e0e242e1b771bc7528df64897e8e8
2489433378@qq.com <2489433378@qq.com>
2023-10-25 13:20:50 +0800

    Initial commit

--- src/力扣热题100/leetcode160.java
+++ src/力扣热题100/leetcode160.java
@@ -1,5 +1,8 @@
 package 力扣热题100;
 
+import java.util.HashSet;
+import java.util.List;
+
 /**
  * @auther XMZ
  * @create 2023--25-12:17
@@ -9,7 +12,7 @@ public class leetcode160 {
     // 普通方法是肯定可以做出来的，哈希集合
     // 你追我赶，我追你赶，很难想也很巧妙
 
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    /*public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
        if(headA == null || headB == null){
            return null;
        }
@@ -19,11 +22,28 @@ public class leetcode160 {
             preA = preA == null ? headB : preA.next;
             preB = preB == null ? headA : preB.next;
        }
-
+       return preB;
     }
 
     public static void main(String[] args) {
         leetcode160 l16 = new leetcode160();
         System.out.println(l16.getIntersectionNode());
+    }*/
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null ){
+            return  null;
+        }
+        HashSet<ListNode> savetemp = new HashSet<>();
+        while (headA != null){
+            savetemp.add(headA);
+            headA = headA.next;
+        }
+        while (headB != null){
+            if(savetemp.contains(headB)){
+                return headB;
+            }
+            headB = headB.next;
+        }
+        return null;
     }
 }
